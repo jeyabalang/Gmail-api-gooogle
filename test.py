4c5e38c5630e221ed8e35e25584ec6ef9504f89d
jeyabalang <jeyabalan360@gmail.com>
2022-05-17 09:27:03 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
  
 from datetime import datetime
 s=input('enter value:')
+date=[]
 for i in range(int(s)):
 	s=i+1
 	print("count"+" "+str(s))
 	my_string = str(input('Enter date(yyyy-mm-dd ): '))
-	my_date = datetime.strptime(my_string, "%Y-%m-%d")
+	my_date1 = datetime.strptime(my_string, "%Y-%m-%d")
 	my_string = str(input('Enter time(hh:mm): ')) 
 	my_date = datetime.strptime(my_string, "%H:%M")
-	print(my_date)
+	date.append(my_date)
+	date.append(my_date1)
+print("Following are the order wise date and time.")
+print(date)
